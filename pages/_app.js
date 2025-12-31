fdef0c140626aa7c5c29275f793ed598c8303c5e
p-karunya <karunya.penumalla@gmail.com>
2025-12-31 11:39:08 -0800

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,25 @@ import { ThemeProvider } from "theme-ui";
 import { SessionProvider } from "next-auth/react";
 import theme from "../theme";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="https://boba.hackclub.com/favicon.png"
+          type="image/png"
+          sizes="32x32"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </>
   );
 }
 
